97021167b1597db94fdf51b19cddcd3292ce3f48
yasirghouri <yasir.ag7@gmail.com>
2021-01-17 21:21:33 +0500

    added banner component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Banner />
       <Row
         title="Netflix Orignals"
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../axios";
 import requests from "../../request";
+import "./Banner.css";
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -18,8 +19,34 @@ const Banner = () => {
     fetchData();
   }, []);
 
-  //   console.log(movie);
-  return <header></header>;
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
 };
 
 export default Banner;
